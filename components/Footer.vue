1eb58a498ff586a29da37a939323632f6a1d6d78
Papa97 <65426684+Papa97@users.noreply.github.com>
2023-06-20 22:37:38 +0200

    first commit

---
+++ components/Footer.vue
@@ -0,0 +1,50 @@
+<script setup></script>
+
+<template>
+    <div id="FooterMain" class="w-full bg-[#e8e8e8] mt-10 mb-2">
+        <div class="max-w-[1200px] mx-auto px-4">
+            <div class="md:flex gap-4 justify-between w-full">
+                <div class="md:w-[50%] pt-4">
+                    <div class="text-lg font-semiold text-[#282828]">Help</div>
+                    <div class="text-[#999] text-sm">
+                        Help Center, Disputes & Reports, Buyer Oritection,
+                        Report IPR infrigment
+                    </div>
+                </div>
+                <div class="md:w-[50%] pt-4">
+                    <div class="text-lg font-semiold text-[#282828]">
+                        AliExpress Multi-Language Sites
+                    </div>
+                    <div class="text-[#999] text-sm">
+                        Russian, Portuguese, Spanish, Italian, French, German
+                    </div>
+                </div>
+            </div>
+
+            <div class="md:flex gap-4 justify-between w-full">
+                <div class="md:w-[50%] pt-4">
+                    <div class="text-lg font-semiold text-[#282828]">
+                        Browse by Category
+                    </div>
+                    <div class="text-[#999] text-sm">
+                        All Popular, Product, Promotion, Low Price
+                    </div>
+                </div>
+                <div class="md:w-[50%] pt-4">
+                    <div class="text-lg font-semiold text-[#282828]">
+                        Alibaba Group
+                    </div>
+                    <div class="text-[#999] text-sm">
+                        Alibaba Group Website, AliExpress
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="FooterSub" class="w-full bg-[#333] text-sm text-gray-400">
+        <div class="max-w-[1200px] mx-auto p-4">
+            Intellectual Property Protection
+        </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import MainLayout from '~/layouts/MainLayout.vue';
+</script>
+
+<style lang="scss" scoped></style>
+
+<template>
+    <MainLayout>
+        <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+            Index Page
+        </div>
+    </MainLayout>
+</template>
